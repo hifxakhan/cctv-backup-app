f695669d0a6a9b507903a736cd7c7097f486d30f
hifxakhan <your-email@example.com>
2026-07-10 21:06:42 +0500

    Deploying on render

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,6 @@
-from app import create_app, socketio
+from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    socketio.run(app, debug=False, host='0.0.0.0', port=5000, use_reloader=False)
+    app.run(debug=True, host='0.0.0.0', port=5000)
